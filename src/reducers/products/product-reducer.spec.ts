46adb5aead213f456321e1f2cf1fadb47ada5781
Alejandro Montes <ango19941994@gmail.com>
2022-06-17 23:35:19 +0200

    Add Test reducer delete

--- src/reducers/products/product-reducer.spec.ts
+++ src/reducers/products/product-reducer.spec.ts
@@ -1,6 +1,6 @@
 import { AnyAction } from '@reduxjs/toolkit';
 import { iProduct } from '../../store/product';
-import { loadProduct } from './action-creator';
+import { addProduct, deleteProduct, loadProduct } from './action-creator';
 import { productsReducer } from './product-reducer';
 
 const mockedArray: Array<iProduct> = [
@@ -19,6 +19,21 @@ describe('Given product reducer', () => {
         test('It should return a new state with that array of characters', () => {
             const newState = productsReducer([], loadProduct(mockedArray));
             expect(newState).toEqual(mockedArray);
+        });
+    });
+    describe('When calling it with add action with a product', () => {
+        test('It should return a new state with the product added to the array', () => {
+            const newState = productsReducer([], addProduct(mockedArray[0]));
+            expect(newState).toEqual(mockedArray);
+        });
+    });
+    describe('When calling it with delete action', () => {
+        test('It should return a new state with the product removed from the array', () => {
+            const newState = productsReducer(
+                mockedArray,
+                deleteProduct(mockedArray[0])
+            );
+            expect(newState).toEqual([]);
         });
     });
 });
